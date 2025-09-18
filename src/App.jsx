2b99a0cdb17b32e1a2be90a22b64e8167d15fb9a
hireche-amine <aminne.email@gmail.com>
2025-09-18 13:13:59 +0100

    fix: correct Search case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import { useState,useEffect } from 'react';
 import { useDebounce } from 'react-use';
 import './App.css';
-import Search from './components/search';
-import Spinner from './components/spinner';
+import Search from './components/Search';
+import Spinner from './components/Spinner';
 import MoviesCard from './components/MovieCard';
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
